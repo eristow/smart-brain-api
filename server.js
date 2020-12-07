3dc1eda7ff1dd5a48b7227c4684480a19aedf6bb
Evan Ristow <evan.ristow1@gmail.com>
2020-12-07 18:41:22 -0500

    Run prettier on files. Add dotenv. Put DB info and Clarifai API key in dotenv

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
@@ -13,25 +14,35 @@ const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'smart-brain',
+  },
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res) => {
+  res.send(db.users);
+});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log('app is running on port 3000');
-})
+});
