15d8afbd13bfd1a2cfbded19d0cdbb0e53dd551e
Evan Ristow <evan.ristow1@gmail.com>
2020-12-20 15:10:47 -0500

    working on adding user sessions with jwt

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,8 +1,8 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined)
-  values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
+INSERT into users (name, age, pet, email, entries, joined)
+  values ('a', 26, 'cat', 'a@a.com', 5, '2018-01-01');
 INSERT into login (hash, email)
-  values ('$2a$10$hpifwFSeOlc0L718Zgeyg.7W/rKbkHKPAxns3qV0WK6DAg02m8OdC', 'jessie@gmail.com');
+  values ('$2a$10$hpifwFSeOlc0L718Zgeyg.7W/rKbkHKPAxns3qV0WK6DAg02m8OdC', 'a@a.com');
 
 COMMIT;
